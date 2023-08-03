8893d5cf7de9c88a6c5dd92c971b94d864dc9649
josh <jglam88@gmail.com>
2023-08-03 14:51:54 -0400

    creates functions to modify destrutively and spread employee object ls

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: "Josh",
+    streetAddress: "438 E 9th"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee}
+    newObj[key] = value
+    return newObj   
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value
+  return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj = {...employee}
+    delete newObj[key]
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
